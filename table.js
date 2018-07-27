38368af80211a8ffbf199bc1f6307d0365cbc584
Amit kumar <amitkumar3968@gmail.com>
2018-07-27 16:36:34 +0530

    adding document

--- table.js
+++ table.js
@@ -13,22 +13,52 @@ console.log(typeof(testfunction()));
 var sqrtnumber = Math.sqrt(29);
 console.log(sqrtnumber);
 
-console.log('-----------checking isprime------------------------');
 
-var inputnumber = 89798;
-var primeflag = 0;
-for(var i = 2; i<= inputnumber/2; i++)
-{ if(inputnumber%i == 0)	
+
+ function isNumberPrimeorNot(inputnumber) 
+{
+//	console.log('-----------checking isprime------------------------');
+	var primeflag = 0;
+	for(var i = 2; i<= Math.sqrt(inputnumber); i++)
+	{ if(inputnumber%i == 0)	
+		{
+			primeflag = 1;
+		}
+	}
+
+	if(primeflag == 1)
 	{
-		primeflag = 1;
+//		console.log('Not a prime number');
+		return false;
+	}else{
+//		console.log('Yay-- ' + inputnumber + '\tis a Prime');
+		return true;
+	}
+
+//		console.log('-----------checking isprime------------Done------------');
+
+//		return (primeflag ? true:false);
+
+}
+//isNumberPrimeorNot(7856);
+
+//var testt = 0/15;
+//console.log(testt);
+
+var userinputnumber = 45;
+var startdate = new Date();
+for(var i=0; i<= Math.sqrt(userinputnumber);i++){
+	
+	if(userinputnumber%i == 0){
+		// it is a factor for the input number. now check is it prime or not ?
+		//console.log(i + ' is a factor for ' + userinputnumber);
+		if(isNumberPrimeorNot(i))
+		{
+			console.log(i + ' is a prime factor of ' + userinputnumber);
+		}
 	}
 }
 
-if(primeflag == 1)
-{
-	console.log('Not a prime number');
-}else{
-	console.log('Yay-- ' + inputnumber + '\tis a Prime')
-}
-
-console.log('-----------checking isprime------------Done------------');
+var enddate = new Date();
+var timediff = Math.abs(enddate.getMilliseconds() - startdate.getMilliseconds());
+console.log(startdate.getMilliseconds(),enddate.getMilliseconds(),timediff);
